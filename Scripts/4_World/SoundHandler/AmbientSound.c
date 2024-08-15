c9cfe5b7dcca0ba8bd0d9ae33f3dda6d8dbb92d6
KOMA <laserdancer1220@gmail.com>
2024-08-15 04:53:49 +0200

    Update AmbientSound.c

--- Scripts/4_World/SoundHandler/AmbientSound.c
+++ Scripts/4_World/SoundHandler/AmbientSound.c
@@ -7,6 +7,7 @@ modded class AmbientHandler
         //Use Corner Top-Left and Corner Bottom-Right Coords
         m_AmbientAreas.Insert(new AmbientArea("AREA NAME/DESCRIPTION", "1000 0 1000", "1000 0 1000", AmbientTimeOfDay.BOTH, {"DS_AmbientMusic_04_SoundSet"}));
 
+        //Global sounds
         daySounds = {"YOUR_MUSIC_SOUNDSETS", "YOUR_MUSIC_SOUNDSETS"};
         nightSounds = {"YOUR_MUSIC_SOUNDSETS", "YOUR_MUSIC_SOUNDSETS"};
 
