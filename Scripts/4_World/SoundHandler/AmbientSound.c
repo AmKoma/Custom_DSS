12ec44a9a92110c2e88f20e9ff9043d243889b6f
KOMA <laserdancer1220@gmail.com>
2025-02-27 09:48:53 +0100

    Update AmbientSound.c
    Added multi-map support

--- Scripts/4_World/SoundHandler/AmbientSound.c
+++ Scripts/4_World/SoundHandler/AmbientSound.c
@@ -7,16 +7,19 @@ modded class AmbientHandler
 
         m_AmbientAreas = new array<ref AmbientArea>;
 
-
-        //Use Corner Top-Left and Corner Bottom-Right Coords
-        // m_AmbientAreas.Insert(new AmbientArea("AREA NAME/DESCRIPTION", "1000 0 1000", "1000 0 1000", AmbientTimeOfDay.BOTH, {"DS_AmbientMusic_04_SoundSet"}));
-        m_AmbientAreas.Insert(new AmbientArea("Tisy", "1259 0 14747", "2506 0 13885", AmbientTimeOfDay.BOTH, {"DS_AmbientMusic_07_SoundSet"}));
-        m_AmbientAreas.Insert(new AmbientArea("NWAF", "3145 0 11857", "5290 0 9438", AmbientTimeOfDay.BOTH, {"DS_AmbientMusic_08_SoundSet", "DS_AmbientMusic_10_SoundSet"}));
+        //Multi map support added
+        if (DSSCoreStatics.GetWorldName() == "chernarus")
+        {
+            //Use Corner Top-Left and Corner Bottom-Right Coords
+            // m_AmbientAreas.Insert(new AmbientArea("AREA NAME/DESCRIPTION", "1000 0 1000", "1000 0 1000", AmbientTimeOfDay.BOTH, {"DS_AmbientMusic_04_SoundSet"}));
+            m_AmbientAreas.Insert(new AmbientArea("Tisy", "1259 0 14747", "2506 0 13885", AmbientTimeOfDay.BOTH, {"DS_AmbientMusic_07_SoundSet"}));
+            m_AmbientAreas.Insert(new AmbientArea("NWAF", "3145 0 11857", "5290 0 9438", AmbientTimeOfDay.BOTH, {"DS_AmbientMusic_08_SoundSet", "DS_AmbientMusic_10_SoundSet"}));
+        }
 
         //Global sounds
         ref array<string> daySounds = {"Test_SoundSet"};
         ref array<string> nightSounds = {"Test_SoundSet"};
-
+        
         //If you want to remove the original global music, uncomment the following two lines
         // DaySoundSets.Clear();
         // DaySoundSets.Clear();
